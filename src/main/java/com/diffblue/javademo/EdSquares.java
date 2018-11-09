eb8cfb86f2ec67132689e68dd5fbc4973d4e9e64
Ed Stenson <edstenson@Eds-MacBook-Pro.local>
2018-11-09 15:03:43 +0000

    Added some exciting maths

---
+++ src/main/java/com/diffblue/javademo/EdSquares.java
@@ -0,0 +1,16 @@
+// Copyright 2016-2018 Diffblue limited. All rights reserved.
+
+package com.diffblue.javademo;
+
+public class EdSquares {
+  /**
+   * Maths madness.
+  */
+  public int doMaths() {
+    int javaMaths = 0;
+    for (int i = 0; i < 9; i++) {
+      javaMaths = i * i;
+    }
+    return javaMaths;
+  }
+}
